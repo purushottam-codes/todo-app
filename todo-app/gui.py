a4438392fce1b62ca1617110a9920cdcb8c367f4
purushottam-codes <puru.banerjee15@gmail.com>
2026-05-04 15:34:30 +0530

    added Complete and Exit button functionality

--- todo-app/gui.py
+++ todo-app/gui.py
@@ -5,11 +5,13 @@ label = fsg.Text("Type in a To-do")
 input_box = fsg.InputText(tooltip="Type in a To-do", key="TODO")
 add_button = fsg.Button("Add")
 list_box = fsg.Listbox(values=functions.get_todo(), key="todos_list",
-                       enable_events=True, size=(45, 10))
+                       enable_events=True, size=(60, 10))
 edit_button = fsg.Button("Edit")
-
+cmplt_button = fsg.Button("Complete")
+exit_button = fsg.Button("Exit")
 window = fsg.Window("My To-do App",
-                    layout=[[label], [input_box], [add_button, edit_button], [list_box]],
+                    layout=[[label], [input_box], [add_button, edit_button, cmplt_button],
+                            [list_box], [exit_button]],
                     font=("Open Sans", 13))
 
 while True:
@@ -22,17 +24,26 @@ while True:
             newTodo = values['TODO'] + "\n"
             todos.append(newTodo)
             functions.set_todo(todos)
-            window["todos_list"].update(todos)
+            window["todos_list"].update(values=todos)
         case "Edit":
             todo_to_edit = values['todos_list'][0]
             new_todo = values['TODO']
             todos = functions.get_todo()
             index = todos.index(todo_to_edit)
-            todos[index] = new_todo + "\n"
+            todos[index] = new_todo
             functions.set_todo(todos)
-            window["todos_list"].update(todos)
+            window["todos_list"].update(values=todos)
+        case "Complete":
+            todos = functions.get_todo()
+            todo_to_complete = values['todos_list'][0]
+            todos.remove(todo_to_complete)
+            functions.set_todo(todos)
+            window["todos_list"].update(values=todos)
+            window["TODO"].update(value="")
         case "todos_list":
-            window["TODO"].update(values['todos_list'][0])
+            window["TODO"].update(value=values['todos_list'][0])
+        case "Exit":
+            break
         case fsg.WIN_CLOSED:
             break
 
